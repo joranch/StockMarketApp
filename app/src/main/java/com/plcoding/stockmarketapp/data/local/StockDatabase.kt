bc963adf7ebb51bbd8397be497c4171bc440beb1
Jøran Haines <joran.haines@gmail.com>
2022-04-27 17:50:14 +0200

    Set up database and dao

---
+++ app/src/main/java/com/plcoding/stockmarketapp/data/local/StockDatabase.kt
@@ -0,0 +1,11 @@
+package com.plcoding.stockmarketapp.data.local
+
+import androidx.room.Database
+
+@Database(
+    entities = [CompanyListingEntity::class],
+    version = 1
+)
+abstract class StockDatabase {
+    abstract val dao: StockDao
+}
---
+++ app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
@@ -0,0 +1,17 @@
+package com.plcoding.stockmarketapp.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface StockApi {
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getListings(
+        @Query("apikey") apiKey: String
+    ) : ResponseBody
+
+    companion object {
+        const val API_KEY = "QTY7YDQB0PJS43MC"
+        const val BASE_URL = "https://www.alphavantage.co/"
+    }
+}
